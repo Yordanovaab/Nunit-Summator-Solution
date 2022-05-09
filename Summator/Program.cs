6d5703ba20a2908f5928abe19428283b7b16a554
Angelina <104991009+Yordanovaab@users.noreply.github.com>
2022-05-09 23:10:43 +0300

    Add the folder with solution

---
+++ Summator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Summator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Summator.Sum(new int[] {1, 2, 3, 4, 5, 6 });
+
+            if (result == 21)
+            {
+                Console.WriteLine("Test PASS");
+            }
+            else
+            {
+                Console.WriteLine("Test FAIL");
+            } 
+                }
+            }
+            
+        }
+    
+
